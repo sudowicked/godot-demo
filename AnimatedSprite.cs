c91a1dc3169365a3f042f7ac425906e89089b4bc
Giannis Nikoloulis <p18niko@ionio.gr>
2025-10-21 01:19:52 +0300

    GTA PIXEL

---
+++ AnimatedSprite.cs
@@ -0,0 +1,31 @@
+using Godot;
+using System;
+
+public class AnimatedSprite : Godot.AnimatedSprite
+{
+    private Timer _time;
+    private Random _random = new Random();
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        _time = GetNode<Timer>("Timer");
+        _time.Connect("timeout", this, nameof(OnTimerTimeout));
+        _time.Start();
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        this.Play("default");
+        GD.Print(_time.WaitTime);
+
+    }
+    
+    private void OnTimerTimeout()
+    {
+        this.FlipH = !this.FlipH;
+        float randomTime = (float)_random.NextDouble();
+        _time.WaitTime = randomTime;
+    }
+}
